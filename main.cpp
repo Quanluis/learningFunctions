282b3b9f8af9159052e270c5392b69ca80edeb2f
Quanluis <quanluis@protonmail.com>
2019-10-09 17:43:09 -0400

    Added calc capabilities using functions to achieve logic.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,114 @@ using namespace std;
 
 void countFunction();
 void countDown();
+void calcFunctionAdd();
+void calcFunctionSub();
+void calcFunctionMul();
+void calcFunctionDiv();
+
+int num1, num2;
 
 int main(){
 
-    countFunction();
-    cout << endl;
-    countDown();
-    
+    //input variable.
+
+    int input;
+
+    // Menu for Calculator 
+
+    cout << "Welcome to my Calculator program." << endl << endl;
+    cout << "Main menu:" << endl << endl;
+
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Exit" << endl;
+
+    cin >> input;
+
+    if(!input){
+        int input;
+        cout << "You must use numbers for input. " << endl;
+    } else{
+        if(input == 1){
+            cout << "You're in addition" << endl;
+            calcFunctionAdd();
+        } else if (input == 2){
+            cout << "you're in subtraction" << endl;
+            calcFunctionSub();
+        } else if (input == 3){
+            cout << "You're in multiplication" << endl;
+            calcFunctionMul();
+        } else if (input == 4){
+            cout << "You're in division" << endl;
+            calcFunctionDiv();
+        } else if(input == 5 ){
+            cout << "Exiting program..." << endl;
+            return 0;
+        }
+    }
 
     return 0;
+}
+
+void calcFunctionAdd(){
+
+    int num1, num2, answer;
+
+    cout << "Give me two numbers and i will add them." << endl;
+
+    cin >> num1; 
+    cin >> num2;
+
+    answer = num1 + num2;
+
+    cout << "The total is " << answer << endl;
+
+}
+
+void calcFunctionSub(){
+    int num1, num2, answer;
+
+    cout << "Give me two numbers and i will subtract them." << endl;
+
+    cin >> num1;
+    cin >> num2;
+
+    answer = num1 - num2;
+
+    cout << "The number is " << answer << endl;
+
+}
+
+void calcFunctionMul(){
+
+    int num1, num2, answer;
+
+    cout << "Give me two numbers and i will multiply them." << endl;
+
+    cin >> num1; 
+    cin >> num2;
+
+    answer = num1 * num2;
+
+    cout << "The total is " << answer << endl;
+
+}
+
+void calcFunctionDiv(){
+
+    int num1, num2, answer;
+
+    cout << "Give me two numbers and i will divide them. :)" << endl;
+
+    cin >> num1; 
+    cin >> num2;
+
+    answer = num1 / num2;
+
+    cout << "The total is " << answer << endl;
+
 }
 
 void countFunction(){
